8722b3808f02f6574cbea547244dd8722cdacd3a
woorim1022 <woorim1022@naver.com>
2021-04-08 13:32:02 +0900

    Update Web/autofarm folder

--- Web/autofarm/autofarm/views.py
+++ Web/autofarm/autofarm/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def base(request):
-    return render(request, 'autofarm/base.html');
+    return render(request, 'autofarm/main.html');
